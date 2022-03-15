53ca08d2991f551dc1245536161a566d7ffd80d0
Piotr Bystranowski <piotr.bystranowski@uj.edu.pl>
2022-03-15 17:34:29 +0100

    scopus class

--- src/get_out_cits.R
+++ src/get_out_cits.R
@@ -51,3 +51,45 @@ print(i)
 print(round(i/length(jours)*100, 2))
 }
 write.csv(venues, "data/cits/out_cits.csv")
+
+venues %>% filter(!is.na(name), name != "Social Science Research Network") %>% 
+ filter(is.na(.$issn)) %>% count(name, sort=T)
+
+
+issn <- venues %>% filter(!is.na(issn), name != "Social Science Research Network") %>% 
+  count(issn, sort=T)
+
+journals_class <- list()
+for (i in 5744:nrow(issn)){
+  url <- paste0("https://api.elsevier.com/content/serial/title?issn=", issn[i, "issn"], "&view=STANDARD&apiKey=bd2c8d4ca7a5db08585fbeedd3b66405&httpAccept=application/json")
+  response <- GET(url)
+  result <- fromJSON(rawToChar(response$content))$`serial-metadata-response`$entry[[1]]
+  if (is.null(result)) {
+    journals_class <- bind_rows(journals_class,
+                                data.frame(
+                                  issn = issn[i, "issn"],
+                                  area = NA))
+  next
+  }
+  if (is.null(result$`subject-area`))  next
+  for (j in 1:length(result$`subject-area`)) {
+    journals_class <- bind_rows(journals_class,
+    data.frame(
+    issn = issn[i, "issn"],
+    tilte = result$`dc:title`,
+    code = result$`subject-area`[[j]]$`@code` ,
+    area = result$`subject-area`[[j]]$`$`))
+  }
+  print(i)
+  
+  }
+
+write.csv(journals_class, "data/cits/scopus_class.csv")
+
+journals_class %>% 
+  filter(!is.na(area)) %>% 
+  group_by(issn) %>% 
+  mutate(weighted = 1/n()) %>% 
+  ungroup() %>% left_join(venues) %>% 
+  group_by(area) %>% summarise(sum = sum(weighted)) %>% 
+  arrange(desc(sum))
